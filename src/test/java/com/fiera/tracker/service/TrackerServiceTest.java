9f3e6f9b3db8eba57d4f3aade5a89f5bf49eaca9
aguilarjuan <juancarlosaguilar06@gmail.com>
2020-11-09 10:12:07 -0300

    project v-0 - commit # 4

--- src/test/java/com/fiera/tracker/service/TrackerServiceTest.java
+++ src/test/java/com/fiera/tracker/service/TrackerServiceTest.java
@@ -1,25 +1,52 @@
 package com.fiera.tracker.service;
 
+import com.fiera.tracker.mapper.TrackerMapper;
+import com.fiera.tracker.message.TrackerDTO;
+import com.fiera.tracker.model.Tracker;
+import com.fiera.tracker.repository.TrackerRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class TrackerServiceTest {
 
+    @MockBean
+    TrackerRepository repository;
+
+    @MockBean
+    TrackerMapper trackerMapper;
+
     @Autowired
     TrackerService trackerService;
 
     @Test
     void createTrackerOK() {
-        assertNotNull(null);
-    }
 
-    @Test
-    void createTrackerFail() {
-        assertNotNull(null);
+        String url = "http://fierastudios.com";
+        Tracker trackerModel = new Tracker();
+        trackerModel.setId("1");
+        trackerModel.setValid(true);
+        trackerModel.setTarget(url);
+        trackerModel.setLink("http://localhost:8080/l/qasd233");
+        Mockito.when(repository.save(Mockito.any(Tracker.class))).thenReturn(trackerModel);
+
+        TrackerDTO trackerDTO = new TrackerDTO();
+        trackerDTO.setValid(true);
+        trackerDTO.setLink("http://localhost:8080/l/qasd233");
+        trackerDTO.setTarget(url);
+        Mockito.when(trackerMapper.toDto(Mockito.any(Tracker.class))).thenReturn(trackerDTO);
+        Mockito.when(trackerMapper.toModel(Mockito.anyString(),Mockito.anyString())).thenReturn(trackerModel);
+
+        TrackerDTO resultObtained = trackerService.createTracker(url);
+
+        assertNotNull(resultObtained);
     }
 
 }
--- src/test/java/com/fiera/tracker/service/TrackerValidatorServiceTest.java
+++ src/test/java/com/fiera/tracker/service/TrackerValidatorServiceTest.java
@@ -9,17 +9,20 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class TrackerValidatorServiceTest {
 
+    final static String URL_VALID = "https://www.fierastudios.com";
+    final static String URL_INVALID = "htttps://fierastudios.com";
+
     @Autowired
     TrackerValidatorService trackerValidatorService;
 
     @Test
-    void validateUrlOK() {
-        assertFalse(trackerValidatorService.validateUrl("123"));
+    void validateUrlIsValid() {
+        assertTrue(trackerValidatorService.validateUrl(URL_VALID));
     }
 
     @Test
-    void validateUrlFail() {
-        assertFalse(!trackerValidatorService.validateUrl("123"));
+    void validateUrlIsNotValid() {
+        assertFalse(trackerValidatorService.validateUrl(URL_INVALID));
     }
 
 }
